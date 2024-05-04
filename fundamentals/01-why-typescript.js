7590636c2e7c81e1842d11381f427db01d37cee7
Rafael Freitas <oraffaudi@gmail.com>
2024-05-04 10:16:51 -0300

    First commit

---
+++ fundamentals/01-why-typescript.js
@@ -0,0 +1,5 @@
+var courseName = "Typescript Bootcamp";
+printCouserName(courseName);
+function printCouserName(name) {
+    console.log("The course name is " + name.toUpperCase());
+}
---
+++ fundamentals/01-why-typescript.ts
@@ -0,0 +1,9 @@
+const courseName = "Typescript Bootcamp";
+
+printCouserName(courseName);
+
+
+function printCouserName(name: string) {
+    console.log("The course name is " + name.toUpperCase());
+
+}
