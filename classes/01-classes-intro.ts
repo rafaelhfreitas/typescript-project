0209489ee053643c6777039518a81001a9aa6c2e
Rafael Freitas <oraffaudi@gmail.com>
2024-05-05 13:03:51 -0300

    Using getter and setter

--- classes/01-classes-intro.ts
+++ classes/01-classes-intro.ts
@@ -1,18 +1,22 @@
 class Course {
 
-    title: string;
-    subtitle: string;
-    creationDate: Date;
-
-
-    constructor( title: string, subtitle: string, creationDate: Date) {
-        this.title = title;
-        this.subtitle = subtitle;
-        this.creationDate = creationDate;
+    constructor( 
+        private _title: string, 
+        private subtitle: string, 
+        private creationDate: Date
+    ) {
 
     }
 
-    age() {
+    set title(newTitle: string){
+        if(!newTitle) {
+            throw "Title cannot be empty";
+        }
+
+        this._title = newTitle;
+    }
+
+    get age() {
         const ageInMs = new Date().getTime() - this.creationDate.getTime();
         return Math.round(ageInMs / 1000 / 60 / 24);
     }
@@ -22,7 +26,14 @@ class Course {
 
 const course = new Course("Typescript Bootcamp", "Learn the language classes", new Date(2000,1,1))
 
+// Error, private member variable
+//course.title = "new value";
 
 
-console.log(course.age())
+course.title = "new value"
+
+console.log(course.age)
+
+
+console.log(course)
 
