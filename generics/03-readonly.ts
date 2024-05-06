003fd4fa61b2d438461500a5db732a38c6e916e6
Rafael Freitas <oraffaudi@gmail.com>
2024-05-05 22:24:57 -0300

    Generic ReadOnly<>

---
+++ generics/03-readonly.ts
@@ -0,0 +1,28 @@
+interface Course {
+    title: string;
+    subtitle: string;
+    lessonsCount: number;
+}
+
+interface ImmutableCourse {
+    readonly title: string;
+    readonly subtitle: string;
+    readonly lessonsCount: number;
+
+}
+
+
+function freezeCourse(course: Course): Readonly<Course> {
+    return Object.freeze(course);
+
+}
+
+
+const frozen = freezeCourse({
+    title: "Typescript Bootcamp",
+    subtitle: "Learn the language, build practical projects",
+    lessonsCount: 100
+})
+
+
+frozen.title = "";
