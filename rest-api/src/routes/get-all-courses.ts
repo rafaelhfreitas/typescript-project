e7f188d5c1cd9cf0d5aec4d087b9ecf74f631c64
Rafael Freitas <oraffaudi@gmail.com>
2024-05-10 01:58:01 -0300

    Added cors

--- rest-api/src/routes/get-all-courses.ts
+++ rest-api/src/routes/get-all-courses.ts
@@ -1,15 +1,28 @@
-import {Request, Response}  from "express";
+import {Request, Response, NextFunction}  from "express";
 import { logger } from "../logger";
 import { AppDataSource } from "../datasource";
 import { Course } from "../models/course";
 
-export async function getAllCourses(request: Request, response: Response) {
+export async function getAllCourses(request: Request, response: Response, next: NextFunction) {
 
-    logger.debug(`Called getAllCousers()`);
 
-    const courses = await AppDataSource.getRepository(Course).createQueryBuilder("courses")
-            .orderBy("courses.seqNo").getMany();
+    try {
+
+        logger.debug(`Called getAllCousers()`);
+
+        const courses = await AppDataSource
+                .getRepository(Course)
+                .createQueryBuilder("courses")
+                .leftJoinAndSelect("courses.lessons", "LESSONS")
+                .orderBy("courses.seqNo").getMany();
+        
     
+        response.status(200).json({courses});        
+        
+    } catch (error) {
+        logger.error(`Error calling getAllCoursers()`);
+        return next(error);
+    }
 
-    response.status(200).json({courses});
+
 }
--- rest-api/src/server.ts
+++ rest-api/src/server.ts
@@ -15,14 +15,22 @@ import { isInteger } from './utils';
 import { logger } from "./logger";
 import { AppDataSource } from "./datasource";
 import { getAllCourses } from "./routes/get-all-courses";
+import { defaultErrorHandler } from "./middlewares/default-error-handler";
 
+
+
+const cors = require("cors");
 
 const app = express();
 
 function setupExpress() {
 
+    app.use(cors({origin:true}));
+
     app.route("/").get(root);
     app.route("/api/courses").get(getAllCourses);
+
+    app.use(defaultErrorHandler);
 
 }
 
