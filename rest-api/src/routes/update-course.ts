b2791148096902cb546e4f2fcd95d6900aeb6b7f
Rafael Freitas <oraffaudi@gmail.com>
2024-05-16 18:06:46 -0300

    endpoint to check user isAdmin

--- rest-api/src/routes/update-course.ts
+++ rest-api/src/routes/update-course.ts
@@ -6,26 +6,6 @@ import { Course } from "../models/course";
 
 
 
-/**
- * 
- * curl -X PATCH  http://localhost:9000/api/courses/76 -H "Content-Type:application/json" -d '{"title": "Typescript Bootcamp v2"}' 
- * curl -X POST  http://localhost:9000/api/courses -H "Content-Type:application/json" -d '{"url": "rafael-bootcamp",
- * "title": "Rafael Bootcamp v2",
- * "iconUrl": "https://angular-university.s3-us-west-1.amazonaws.com/course-images/typescript-bootcamp-2.jpg",
- * "longDescription": "Learn in depth the Typescript language, build practical real-world projects",
- * "category": "BEGINNER"}' 
- * curl -X DELETE http://localhost:9000/api/courses/77 -H "Content-Type:application/json" 
- *  
- * curl -X POST  http://localhost:9000/api/users -H "Content-Type:application/json" -d '{"email": "rafael@rafael.com", "pictureUrl":"https://avatars.githubusercontent.com/u/5454709", "password":"123", "isAdmin": false}'
- * 
- * curl -X GET  http://localhost:9000/api/courses;
- * 
- * curl -X POST  http://localhost:9000/api/login -H "Content-Type:application/json" -d '{"email": "test@angular-university.io", "password":"test"}'
- * curl -X POST  http://localhost:9000/api/login -H "Content-Type:application/json" -d '{"email": "rafael@rafael.com", "password":"123"}'
- * 
- * curl -X GET  http://localhost:9000/api/courses -H "Authorization:eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJ1c2VySWQiOjEsImVtYWlsIjoidGVzdEBhbmd1bGFyLXVuaXZlcnNpdHkuaW8iLCJpc0FkbWluIjpmYWxzZSwiaWF0IjoxNzE1ODkxNzMzfQ.dIaaatfphFCchUknXKbHtjmFZ-0OG68-0Jxym7OQ2bc"
- */
-
 export async function updateCourse(request: Request, response: Response, next: NextFunction){
 
 
